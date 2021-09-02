695a1a0ae0fd92c6fbff879920c03b4a0b0c4221
John Avitable <john.avitable@gmail.com>
2021-09-02 09:53:27 -0400

    adding line plot for projection

--- app/bbt.py
+++ app/bbt.py
@@ -1,4 +1,6 @@
 import bbdata as bb
+import matplotlib.pyplot as plt
+import pandas as pd
 
 projEnd = '2021-12-31'
 
@@ -6,4 +8,9 @@ expenseacct = bb.acct('Expense')
 
 expenseproj = expenseacct.projRev(projEnd)
 
+expDf = expenseproj[2]
+
 print("Expense account projected balance for",projEnd,"will be $",expenseproj[0])
+
+expDf.plot(kind='line',x='Date',y='Balance',title='Expense account projected balances')
+plt.savefig('./proj_output/output.png')
