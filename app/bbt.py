8614bcbbd5482bb211ea777600d36494d632e124
John Avitable <john.avitable@gmail.com>
2021-09-03 22:03:54 -0400

    webpage for graph

--- app/bbt.py
+++ app/bbt.py
@@ -1,6 +1,10 @@
 import bbdata as bb
 import matplotlib.pyplot as plt
 import pandas as pd
+import http.server
+import socketserver
+import os
+
 
 projEnd = '2021-12-31'
 
@@ -14,3 +18,19 @@ print("Expense account projected balance for",projEnd,"will be $",expenseproj[0]
 
 expDf.plot(kind='line',x='Date',y='Balance',title='Expense account projected balances')
 plt.savefig('./proj_output/output.png')
+
+PORT = 8080
+DIRECTORY = "proj_output"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+try:
+    while True:
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("Serving output at http://localhost:%s" % PORT)
+            httpd.serve_forever()
+except KeyboardInterrupt:
+    print("Press Ctrl-C to terminate")
+    httpd.server_close()
