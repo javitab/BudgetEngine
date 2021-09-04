8196d3296e266f775d03d68b7fe5c1103057f65e
John Avitable <john.avitable@gmail.com>
2021-09-04 14:22:30 -0400

    add menu and print expenses

--- app/bb-importexpenses.py
+++ app/bb-importexpenses.py
@@ -1,4 +1,5 @@
 import bbdata as bb
+import pprint as pp
 
 def insertExpense(acctID, DayOfMonth, Name, Amount, StartDate, EndDate):
     filter = {"Name":Name}
@@ -19,19 +20,30 @@ def insertExpense(acctID, DayOfMonth, Name, Amount, StartDate, EndDate):
         x = bb.expenses.insert_one(expenseToWrite)
         print(x.inserted_id)
 
-print("Please enter info for new expense:")
-bb.printAsDataFrame(bb.listCollection("accounts"))
-inputacctID = input("Please enter account: ")
-inputName = input("Please enter name of new expense: ")
-inputAmount = input("Please enter the amount: ")
-AmountInt = float(inputAmount)
-inputStartDate = input("Please enter a start date (YYYY-MM-DD): ")
-StartDateFormatted = bb.convDate(inputStartDate)
-DayOfMonth = StartDateFormatted.day
-print("Interpreting DayOfMonth as %s from StartDate " % StartDateFormatted.day)
-inputEndDate = input("Please enter the EndDate (if applicable): ")
-if inputEndDate == "":
-    EndDateFormatted = None
-elif inputEndDate != "":
-    EndDateFormatted = bb.convDate(inputEndDate)
-insertExpense(inputacctID,DayOfMonth,inputName,AmountInt,StartDateFormatted,EndDateFormatted)
+print("Available actions:")
+print("1: Print expenses for account")
+print("2: Add new expense")
+action = input("What would you like to do? ")    
+
+if action == '1':
+    bb.printAsDataFrame(bb.listCollection("accounts"))
+    inputacctID = input("Please enter account: ")
+    bb.printAsDataFrame(bb.listExpenses(inputacctID))
+
+if action == '2':
+    print("Please enter info for new expense:")
+    bb.printAsDataFrame(bb.listCollection("accounts"))
+    inputacctID = input("Please enter account: ")
+    inputName = input("Please enter name of new expense: ")
+    inputAmount = input("Please enter the amount: ")
+    AmountInt = float(inputAmount)
+    inputStartDate = input("Please enter a start date (YYYY-MM-DD): ")
+    StartDateFormatted = bb.convDate(inputStartDate)
+    DayOfMonth = StartDateFormatted.day
+    print("Interpreting DayOfMonth as %s from StartDate " % StartDateFormatted.day)
+    inputEndDate = input("Please enter the EndDate (if applicable): ")
+    if inputEndDate == "":
+        EndDateFormatted = None
+    elif inputEndDate != "":
+        EndDateFormatted = bb.convDate(inputEndDate)
+    insertExpense(inputacctID,DayOfMonth,inputName,AmountInt,StartDateFormatted,EndDateFormatted)
