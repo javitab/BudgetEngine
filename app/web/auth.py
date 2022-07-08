ef4dfbe98fded1e5a7c0cd090a52db6babf44603
John Avitable <john.avitable@gmail.com>
2022-07-07 20:05:48 -0400

    flask web frontend

--- app/web/auth.py
+++ app/web/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint,render_template, request
+from flask import Blueprint,render_template, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -14,4 +15,22 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email=request.form.get('email')
+        firstName=request.form.get('firstName')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 characters', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 7 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            pass
+        else: 
+            flash('Account Created!', category='success')
     return render_template("signup.html")
